ad5956c90958d67256aed91015eab2579ef5f9b4
Jit Debnath <110807878+git-jit0@users.noreply.github.com>
2023-09-11 10:01:12 +0530

    Add files via upload

---
+++ LCM_GCD.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+// Function prototypes
+int LCM(int a, int b);
+int GCD(int a, int b);
+
+int main() {
+    int a, b;
+    printf("Enter two numbers to find GCD and LCM: ");
+    scanf("%d %d", &a, &b);
+
+    int lcm = LCM(a, b);
+    int gcd = GCD(a, b);
+
+    printf("LCM is %d\n", lcm);
+    printf("GCD is %d\n", gcd);
+    return 0;
+}
+
+int LCM(int a, int b) {
+    int max = (a > b) ? a : b;
+    int lcm = max;
+
+    for(int i= lcm;i<=a*b;i++)
+    {
+        if (lcm % a == 0 && lcm % b == 0) 
+        {
+            return lcm;
+            break;
+        }
+        
+    }
+}
+
+int GCD(int a, int b) 
+{
+    int max =a>b?b:a;
+    int gcd = max;
+    for(int i=max;i>=1;i--)
+        {
+            if(a%i==0&&b%i==0)
+                {
+                    return gcd;
+                    break;
+                }
+        }
+
+    return a;
+}
