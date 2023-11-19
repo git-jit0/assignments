edfe6848ed3e525dd386ca7b2f9e7e56997745d2
Jit Debnath <110807878+git-jit0@users.noreply.github.com>
2023-11-19 11:41:16 +0530

    Update swap1.c

--- swap1.c
+++ swap1.c
@@ -5,15 +5,11 @@ int main()
     printf("Enter two numbers: ");
     scanf("%d%d",&a,&b);
 
-    // a b , c=a a=b b=c;
-    c=a; a=b; b=c;
+    c=a;
+    a=b;
+    b=c;
 
     printf("The number is %d%d and swap is %d%d",b,a,a,b);
-    /*
-    easy way to do it, 
-    swap in print.
-    am i right?
-    */
     
     return 0;
 }
